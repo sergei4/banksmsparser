cf5ff2058ae5c78ec91359d325ee113a0c3d4f5d
Mihail Eremkin <developer@skm-mobile.com>
2018-06-07 22:29:12 +0300

    upd. Доработка шаблонов promsvyazbank

--- src/test/java/com/skmmobile/banksmsparser/bank/PromsvyazbankSmsTest.java
+++ src/test/java/com/skmmobile/banksmsparser/bank/PromsvyazbankSmsTest.java
@@ -18,7 +18,6 @@ public class PromsvyazbankSmsTest extends AbsXmlBankSmsTest {
                 ""
         );
         checkBankSms(
-                parser,
                 "Karta *2006: Perevod s karty na kartu - spisanie 10 000.00 RUR; 16/01, 11:14;PSBANK.RU;CARD2CARD PSB MOBILE.Dostupno 4 595.50 RUR.",
                 "perevod",
                 "2006",
@@ -26,7 +25,13 @@ public class PromsvyazbankSmsTest extends AbsXmlBankSmsTest {
                 ""
         );
         checkBankSms(
-                parser,
+                "Karta *2006: Perevod s karty na kartu - spisanie 10 000,00 RUR; 16/01, 11:14;PSBANK.RU;CARD2CARD PSB MOBILE.Dostupno 4 595.50 RUR.",
+                "perevod",
+                "2006",
+                "10000",
+                ""
+        );
+        checkBankSms(
                 "Kontrakt *3338: Zachislenie 34 500.00 RUR;2018-01-19.Dostupno 35 041.50 RUR.",
                 "zachislenie",
                 "Kontrakt *3338",
@@ -34,15 +39,6 @@ public class PromsvyazbankSmsTest extends AbsXmlBankSmsTest {
                 ""
         );
         checkBankSms(
-                parser,
-                "Kontrakt *3338: Zachislenie 34 500.00 RUR;2018-01-19.Dostupno 35 041.50 RUR.",
-                "zachislenie",
-                "Kontrakt *3338",
-                "34500",
-                ""
-        );
-        checkBankSms(
-                parser,
                 "Karta *8139: Snyatie nalichnyh v bankomate 4 700.00 RUR; 11/01, 19:09;SAMARA;ATM 10150168 304A KIRO.Dostupno 42.78 RUR.",
                 "cash_atm",
                 "8139",
@@ -61,6 +57,55 @@ public class PromsvyazbankSmsTest extends AbsXmlBankSmsTest {
                 "expense",
                 "2006",
                 "1298.12",
+                ""
+        );
+        checkBankSms(
+                "Karta *5131: Vnesenie sredstv 2 793.97 RUR; 03/05, 13:47;MOSCOW;PSB-RETAIL 00777781.Dostupno 15 994,59 RUR (v t.ch. kredit 15 000,00 RUR).",
+                "popolnenie",
+                "5131",
+                "2793.97",
+                ""
+        );
+        checkBankSms(
+                "Karta *5131: Vnesenie sredstv 2 793,97 RUR; 03/05, 13:47;MOSCOW;PSB-RETAIL 00777781.Dostupno 15 994,59 RUR (v t.ch. kredit 15 000,00 RUR).",
+                "popolnenie",
+                "5131",
+                "2793.97",
+                ""
+        );
+        checkBankSms(
+                "Kontrakt *5340: Zachislenie 86 358,53 RUR;2018-04-28.Dostupno 101 648,53 RUR  (v t.ch. kredit 15 000,00 RUR).",
+                "zachislenie",
+                "Kontrakt *5340",
+                "86358.53",
+                ""
+        );
+        checkBankSms(
+                "Karta *8139: Snyatie nalichnyh v bankomate 4 700.00 RUR; 11/01, 19:09;SAMARA;ATM 10150168 304A KIRO.Dostupno 42.78 RUR.",
+                "cash_atm",
+                "8139",
+                "4700",
+                ""
+        );
+        checkBankSms(
+                "Karta *2213: Snyatie nalichnyh v bankomate 200 000,00 RUR; 18/04, 19:14;MOSCOW;Alfa Iss 98 DMITROVSKO.Dostupno 88 529,99 RUR.",
+                "cash_atm",
+                "2213",
+                "200000",
+                ""
+        );
+        checkBankSms(
+                "Karta *5131: Beznalichnoe spisanie 68 000.00 RUR; 28/04, 15:29;MOSCOW;00887750.Dostupno 33 427,53 RUR (v t.ch. kredit 15 000,00 RUR).",
+                "unknown",
+                "5131",
+                "68000",
+                ""
+        );
+        checkBankSms(
+                "Karta *5131: Beznalichnoe spisanie 68 000,00 RUR; 28/04, 15:29;MOSCOW;00887750.Dostupno 33 427,53 RUR (v t.ch. kredit 15 000,00 RUR).",
+                "unknown",
+                "5131",
+                "68000",
                 ""
         );
     }
